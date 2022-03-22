a0c38cbdfd4bdf0b1205372e58aa1ed03c00347d
Brian-15 <bcirieco@gmail.com>
2022-03-21 19:19:43 -0700

    add user routes

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,9 +1,52 @@
-var express = require('express');
-var router = express.Router();
+const { Router } = require("express");
+const router = Router();
+const { User } = require("../models");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await User.findAll();
+    return res.status(200).json(users);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/* GET user listing. */
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findOne({
+      where: { id },
+      include: "words"
+    });
+    return res.status(200).json(user);
+  } catch(err) {
+    return next(err);
+  }
+});
+
+/* POST users listing. */
+router.post("/", async (req, res, next) => {
+  const { username, displayName, password } = req.body;
+  try {
+    const user = await User.create({ username, displayName, password });
+    return res.status(201).json(user);
+  } catch(err) {
+    return next(err);
+  }
+});
+
+/* DELETE user listing. */
+router.delete("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findOne({ where: { id } });
+    await user.destroy();
+    return res.status(200).json({ message: "User has been deleted" });
+  } catch(err) {
+    return next(err);
+  }
 });
 
 module.exports = router;
