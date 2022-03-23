343e4ce20b108e8df5e0964940e2d321ba50fcc4
Brian-15 <bcirieco@gmail.com>
2022-03-22 17:49:11 -0700

    changed /words GET route to reflect new model hierarchy & to call external API to populate definitions table

--- backend/src/routes/words.js
+++ backend/src/routes/words.js
@@ -1,14 +1,38 @@
 const express = require("express");
 const router = express.Router();
-const { Word } = require("../models");
+const { Word, Definition, Category } = require("../models");
+
+const axios = require("axios");
+const API_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
 router.get("", async (req, res, next) => {
   const term = req.query.term;
   try {
-    const wordBank = term
-      ? await Word.findOne({ where: { word: term } })
-      : await Word.findAll();
-    return res.status(200).json(wordBank);
+    if (!term) return res.status(200).json(await Word.findAll());
+
+    let word = await Word.findOne({ where: { word: term } });
+    if (!word.definitions.length) {
+      // parse definition data from Free Dictionary API https://dictionaryapi.dev/
+      const definitions = await axios.get(API_URL + term);
+      definitions.forEach(({ meanings }) => {
+        meanings.forEach(({ partOfSpeech, definitions }) => {
+          let category = await Category.findOne({ where: { name: partOfSpeech } });
+          if (!category) {
+            category = await Category.create({ partOfSpeech });
+          }
+          definitions.forEach(({ definition, example }) => {
+            await Definition.create({
+              definition,
+              example,
+              typeId: category.id,
+              wordId: word.id
+            });
+          });
+        });
+      });
+      word = await Word.findOne({ where: { word: term } });
+    }
+    return res.status(200).json(word);
   } catch (err) {
     return next(err);
   }
