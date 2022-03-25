762890e65b0b47fdf84f039e031898d251353ab1
Brian-15 <bcirieco@gmail.com>
2022-03-25 15:56:58 -0700

    add integration tests for /words route, and added promise chain on word route to populate definitions and categories before fetching updated word object

--- backend/src/routes/words.js
+++ backend/src/routes/words.js
@@ -1,40 +1,57 @@
 const express = require("express");
 const router = express.Router();
-const { Word, Definition, Category } = require("../models");
-
-const axios = require("axios");
-const API_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const { Word, Category, Definition } = require("../models");
+const loadDefinitions = require("../helpers/fetchDefinitions");
 
 router.get("", async (req, res, next) => {
   const term = req.query.term;
   try {
     if (!term) return res.status(200).json(await Word.findAll());
-
     let word = await Word.findOne({
       where: { word: term },
-      include: "definitions"
+      include: {
+        model: Definition,
+        as: "definitions",
+        include: {
+          all: true,
+          nested: true
+        }
+      }
     });
-    
+
     if (!word.definitions.length) {
       // parse definition data from Free Dictionary API https://dictionaryapi.dev/
-      const definitions = await axios.get(API_URL + term);
-      definitions.forEach(({ meanings }) => {
-        meanings.forEach(({ partOfSpeech, definitions }) => {
-          let category = await Category.findOne({ where: { name: partOfSpeech } });
-          if (!category) {
-            category = await Category.create({ partOfSpeech });
-          }
-          definitions.forEach(({ definition, example }) => {
-            await Definition.create({
-              definition,
-              example,
-              categoryId: category.id,
-              wordId: word.id
-            });
+      const wordId = word.id;
+      word = await Promise.resolve(
+        loadDefinitions(term)
+        .then(async result => {
+          return [
+            await Promise.all(Object.keys(result)
+              .map(category => Category.create({ name: category }))),
+            result
+          ];
+        }).then(([categories, definitions]) => {
+          const defModels = [];
+          categories.forEach(({ id, name }) => {
+            defModels.push(...definitions[name].map(data => {
+              return Definition.create({
+                wordId,
+                categoryId: id,
+                ...data
+              });
+            }));
           });
-        });
-      });
-      word = await Word.findOne({ where: { word: term }, include: "definitions" });
+          return Promise.all(defModels);
+        }).then(() => {
+          return Word.findOne({
+            where: { word: term },
+            include: {
+              all: true,
+              nested: true
+            }
+          });
+        })
+      );
     }
     return res.status(200).json(word);
   } catch (err) {
